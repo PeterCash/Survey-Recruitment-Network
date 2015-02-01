10ac0e3283cad345384d63d5938f69bb5fe4c820
Peter Cash <cashp@aston.ac.uk>
2015-02-01 19:59:40 +0000

    get age and dob

--- classes/profilefunctions.php
+++ classes/profilefunctions.php
@@ -17,13 +17,27 @@ class profilefunctions
     }
 
 
-    public function getAge($id)
+    public function getDateOfBirth($id)
     {
       $this->db->query("SELECT * FROM user_profiles WHERE user_id=?", array($id));
 
         if (isset($this->db->first()->user_id)) {
-            return $this->db->first()->user_id;
+            $date =  $this->db->first()->date_of_birth;
+            return date('jS F Y', strtotime($date));
         }
+
+    }
+
+    public function getAge($id)
+    {
+        $now = new DateTime(null, new DateTimeZone('Europe/London'));
+
+        $dob = $this->getDateOfBirth($id);
+
+        $today = date("D M y");
+
+
+        return $today - $dob;
 
     }
 
