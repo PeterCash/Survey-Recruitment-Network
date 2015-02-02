0789942287504be43a5932dd3c419f7c34bb848e
PeterCash <cashp@aston.ac.uk>
2015-02-02 16:25:45 +0000

    added ajax to login

--- index.php
+++ index.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <title>Survey Recruitment Network</title>
     <link rel="stylesheet" type="text/css" href="content/main.css">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
+    <script src="http://malsup.github.com/jquery.form.js"></script>
+    <script src="scripts/login.js"></script>
+
 </head>
 <body>
 <h1>Survey Distribution Network</h1>
 <br/>
 <br/>
 
-<div id="login" class="login" >
+<div name="login" id="login" class="login" >
     <form action="content/loginauthenticator.php" method="post">
         <input name="username" id="username" type="text"  title="Username">
         <br/>
@@ -19,6 +24,8 @@
         <input name="submit" id="LoginSubmit" type="submit">
     </form>
 </div>
+
+<div id="ajaxStuff"></div>
 
 </body>
 
