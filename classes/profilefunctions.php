94646f1e7b1cb0d9ab3430c0fef10ec6910d9226
PeterCash <cashp@aston.ac.uk>
2015-02-04 16:14:43 +0000

    Tried putting interest finding in own class

--- classes/profilefunctions.php
+++ classes/profilefunctions.php
@@ -6,9 +6,13 @@
  * Time: 18:01
  */
 require_once '../core/settings.php';
+
 class profilefunctions
+
 {
     protected $db = null;
+
+
 
 
     public function __construct($database)
@@ -28,19 +32,6 @@ class profilefunctions
 
     }
 
-    public function getAge($id)
-    {
-        $this->db->query("SELECT * FROM user_profiles WHERE user_id=?", array($id));
-
-        if (isset($this->db->first()->user_id)) {
-            $date =  new DateTime($this->db->first()->date_of_birth);
-            $today = new DateTime('today');
-            // $today->format('Y-m-d H:i:s');
-            // '<br/>';
-            return $date->diff($today)->y;
-        }
-    }
-
     public function convertDate($date)
     {
         return date('jS F Y', strtotime($date));
@@ -57,6 +48,80 @@ class profilefunctions
             return $this->db->first()->label;
         }
     }
+
+    public function getAge($id)
+    {
+        $this->db->query("SELECT * FROM user_profiles WHERE user_id=?", array($id));
+
+        if (isset($this->db->first()->user_id)) {
+            $date =  new DateTime($this->db->first()->date_of_birth);
+            $today = new DateTime('today');
+            // $today->format('Y-m-d H:i:s');
+            // '<br/>';
+            return $date->diff($today)->y;
+        }
+    }
+
+
+
+    //Get Interests
+
+
+    function getChildren($root, $db)
+    {
+
+        $vChildTopics = $db->query("SELECT * FROM interests WHERE parent=?", array($root));
+
+
+        $interestList = '';
+
+        foreach ($vChildTopics->results() as $topic) {
+
+
+            $interestList .= '<li>' . ($this->isParent($topic->interest_id, $db)) ? $this-> $topic->interest : getChildren($topic->interest_id,$db) . '</li>';
+
+      /*      if ($this->isParent($topic->interest_id, $db)) {
+                array_push($this->$interestList, $topic->interest);
+                array_push($this->$interestList, $this->getChildren($topic->interest_id, $db, $output));
+            }else{
+                array_push($this->$interestList, $topic->interest);
+            }*/
+
+
+        }//End For Loop
+
+        return '<ul>' . $interestList . '</ul>';
+
+
+    }
+
+    function isParent($root, $db)
+    {
+        $vIsParent = $db->query("SELECT * FROM interests WHERE parent=?", array($root));
+
+        if ($vIsParent->count() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function userInterest($interestID, $db)
+    {
+        $uid = $_SESSION['uid'];
+        $vUserInterests = $db->query("SELECT * FROM userinterests WHERE user_id=? AND interest_id=?", array($uid, $interestID));
+
+        if ($vUserInterests->count() < 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    //End get interests
+
+
+
 
     public function getSurveysCreated()
     {
--- content/profile.php
+++ content/profile.php
@@ -31,71 +31,7 @@ $db = $database;
 $profile = new profilefunctions($db);
 echo $profile->getAge($_SESSION['uid']);
 
-function isParent($root, $db)
-{
-    $vIsParent = $db->query("SELECT * FROM interests WHERE parent=?", array($root));
 
-    if ($vIsParent->count() > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function getChildren($root, $db)
-{
-
-    $vChildTopics = $db->query("SELECT * FROM interests WHERE parent=?", array($root));
-
-    foreach ($vChildTopics->results() as $topic) {
-
-        /*All this ford is echo checkboxes according to whether they relate to a topic a user has selected.
-         What this means is that a checked box is a selected option and an unchecked box is not selected.
-         */
-
-        if (!isParent($topic->interest_id, $db)) {
-            if (userInterest($topic->interest_id, $db) == true) {
-                echo '<input type="checkbox" name="interests[]" id="' . $topic->interest_id . '" value="' . $topic->interest . '" checked>';
-            } else {
-                echo '<input type="checkbox" name="interests[]" id="' . $topic->interest_id . '" value="' . $topic->interest . '">';
-            }
-
-        }
-
-        /*This echos out the interest name only. If a topic is a parent then the topic itself cannot be
-        selected itself only it's children can be selected.
-        */
-        echo $topic->interest;
-        echo "</br>";
-
-        /*
-* If a topic has a non-zero number of children then the function will call itself using the
-* interest as it's root. This means that the function can keep running inside itself until all
-* interests are returned.
-* */
-
-        if (isParent($topic->interest_id, $db)) {
-            echo '<ul>';
-            getChildren($topic->interest_id, $db);
-            echo '</ul>';
-        }
-
-
-    }
-}
-
-
-function userInterest($interestID, $db)
-{
-    $uid = $_SESSION['uid'];
-    $vUserInterests = $db->query("SELECT * FROM userinterests WHERE user_id=? AND interest_id=?", array($uid, $interestID));
-
-    if ($vUserInterests->count() < 1) {
-        return false;
-    } else {
-        return true;
-    }
-}
 ?>
 
 
@@ -104,7 +40,7 @@ function userInterest($interestID, $db)
 <form action="updateinterests.php" method="post" name = "interestsForm" id="interestsForm">
 
 <?php
-getChildren(0, $database);
+$profile->getChildren(0, $database);
 ?>
 
 <input name="submit" id="submitButton" type="submit" value="Update Interests">
